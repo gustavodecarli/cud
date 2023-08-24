beee2b66aa7793e42e5072d133688d78354e8437
Gustavo De Carli <gustavodecarli@gmail.com>
2023-08-24 01:03:45 -0300

    Agreado de search

--- app/configurate/page.tsx
+++ app/configurate/page.tsx
@@ -1,7 +1,9 @@
 'use client';
 
+import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { Card, Flex, Grid, Title } from '@tremor/react';
 import Link from 'next/link';
+import { useState } from 'react';
 
 const data = [
   {
@@ -39,27 +41,112 @@ const data = [
   {
     name: 'Genero',
     route: 'genero'
+  },
+  {
+    name: 'TipoTelefono',
+    route: 'tipotelefono'
+  },
+  ,
+  {
+    name: 'TipoEquipamiento',
+    route: 'tipoequipamiento'
+  },
+  ,
+  {
+    name: 'TipoDeficiencia',
+    route: 'tipodeficiencia'
+  },
+  ,
+  {
+    name: 'TipoOrientacionProfesional',
+    route: 'tipoorientacionprofesional'
+  },
+  ,
+  {
+    name: 'TipoVivienda',
+    route: 'tipovivienda'
+  },
+  ,
+  {
+    name: 'EstadoCalle',
+    route: 'estadocalle'
+  },
+  ,
+  {
+    name: 'Vinculos',
+    route: 'vinculos'
+  },
+  ,
+  {
+    name: 'GradoAlfabetizacion',
+    route: 'gradoalfabetizacion'
+  },
+  ,
+  {
+    name: 'CondicionActividad',
+    route: 'condicionactividad'
+  },
+  ,
+  {
+    name: 'CategoriaActividad',
+    route: 'categoriaactividad'
   }
-  , {  name: 'TipoTelefono',  route: 'tipotelefono' }, , {  name: 'TipoEquipamiento',  route: 'tipoequipamiento' }, , {  name: 'TipoDeficiencia',  route: 'tipodeficiencia' }, , {  name: 'TipoOrientacionProfesional',  route: 'tipoorientacionprofesional' }, , {  name: 'TipoVivienda',  route: 'tipovivienda' }, , {  name: 'EstadoCalle',  route: 'estadocalle' }, , {  name: 'Vinculos',  route: 'vinculos' }, , {  name: 'GradoAlfabetizacion',  route: 'gradoalfabetizacion' }, , {  name: 'CondicionActividad',  route: 'condicionactividad' }, , {  name: 'CategoriaActividad',  route: 'categoriaactividad' }, /*MENUS*/
+  /*MENUS*/
 ];
 
 export default function PlaygroundPage() {
+  const [search, setSearch] = useState(data);
+
+  function handleSearch(value: string): void {
+    setSearch(
+      data.filter((e) => e?.name.toLowerCase().includes(value.toLowerCase()))
+    );
+  }
+
   return (
     <main className="p-4 md:p-10 mx-auto max-w-7xl">
+      <div className="flex flex-row">
+        <div className="relative my-5 justify-stretch">
+          <label htmlFor="search" className="sr-only">
+            Buscar
+          </label>
+          <div className="rounded shadow-sm w-64">
+            <div
+              className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3"
+              aria-hidden="true"
+            >
+              <MagnifyingGlassIcon
+                className="mr-3 h-4 w-4 text-gray-400"
+                aria-hidden="true"
+              />
+            </div>
+            <input
+              type="text"
+              name="search"
+              id="search"
+              className="h-10 block w-full rounded-md border border-gray-200 pl-9 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+              placeholder="Buscar menu"
+              spellCheck={false}
+              onChange={(e) => handleSearch(e.target.value)}
+            />
+          </div>
+        </div>
+      </div>
       <Grid numItemsSm={2} numItemsLg={3} className="gap-6">
-        {data.map((item) => (
-          <Link key={item.name} href={item.route}>
-            <Card key={item.name} decoration="top" decorationColor="indigo">
-              <Flex
-                justifyContent="center"
-                alignItems="baseline"
-                className="space-x-2"
-              >
-                <Title>{item.name}</Title>
-              </Flex>
-            </Card>
-          </Link>
-        ))}
+        {search &&
+          search.map((item) => (
+            <Link key={item?.name} href={item ? item.route : ''}>
+              <Card key={item?.name} decoration="top" decorationColor="indigo">
+                <Flex
+                  justifyContent="center"
+                  alignItems="baseline"
+                  className="space-x-2"
+                >
+                  <Title>{item?.name}</Title>
+                </Flex>
+              </Card>
+            </Link>
+          ))}
       </Grid>
     </main>
   );
